db2b4c487391623eeba6841c48479cc23ab82d32
Roshan Sreekanth <roshan.sreekanth@mycit.ie>
2020-03-15 18:01:46 +0000

    Updated homepage of default application and added a ThemeData file

---
+++ lib/Styling/AppTheme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData appTheme()
+{
+  return ThemeData
+  (
+    primaryColor: Colors.red[900],
+    brightness: Brightness.dark,
+    accentColor: Colors.red[900],
+  );
+}
